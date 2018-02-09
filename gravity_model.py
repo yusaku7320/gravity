81ed567a9362720dbbcc42a37e07d706cbd52304
佃優作 <yusakutsukuda@tsukudayuusakunoMacBook-puro.local>
2018-02-09 09:00:28 +0900

    重力モデル実装

--- gravity_model.py
+++ gravity_model.py
@@ -2,14 +2,16 @@ import random
 import math
 import pandas as pd
 
-def gravity_model():
-    pass
+def gravity_model(population1,population2,distance):
+    G = 1 #重力定数
+    traffic = (G * population1 * population2) / (distance ** 2)
+    return traffic
 
 class prefectur():
     def __init__(self,ken):
         self.name = ken[0]
         self.population = ken[1]
-        self.distance_list = {}
+        self.distance = {}
         self.traffic = {}
     def __repr__(self):
         return str(self.__dict__)
@@ -28,7 +30,17 @@ if __name__ == '__main__':
         for j in distance_list.values.tolist():
             name = j[0]
             distance = j[id+1]
-            ken.distance_list[name] = distance
+            ken.distance[name] = distance
     for i in list:
         print(i.name)
-        print(i.distance_list)
+        print(i.distance)
+
+    for ken1 in list:
+        for ken2 in list:
+            if ken1.name == ken2.name:
+                ken1.traffic[ken2.name] = 0
+            else:
+                ken1.traffic[ken2.name] = gravity_model(ken1.population,ken2.population,ken1.distance[ken2.name])
+    for i in list:
+        print(i.name)
+        print(i.traffic)
